afd8c2e3f80d2151c27fdbd302a613eb6b43894f
Javier Lopez <javierlopez_85@hotmail.com>
2021-11-16 00:52:17 -0300

    ball and platform made

--- src/game.js
+++ src/game.js
@@ -1,75 +1,98 @@
 export class Game extends Phaser.Scene {
-		constructor() {
-				super({ key: 'game'});
-				this.platform = [];
-				this.balls = [];
-				this.cursors;
-		}
+    constructor() {
+	super({ key: 'game'});
+	this.platform = [];
+	this.balls = [];
+	this.cursors;
+    }
 
-		
-		
-		//Platform creating function
-		makeRectangle(description) {
-				const height = 10;
-				let gameRectangle = this.physics.add.existing(
-						this.add.rectangle(
-								description.x,
-								description.y,
-								description.width,
-								height,
-								description.color));
-				gameRectangle.body.allowGravity = false;
-				return gameRectangle;
-		}
+    
+    
+    //Platform creating function
+    makeRectangle(description) {
+	const height = 10;
+	let gameRectangle = this.physics.add.existing(
+	    this.add.rectangle(
+		description.x,
+		description.y,
+		description.width,
+		height,
+		description.color));
+	gameRectangle.body.allowGravity = false;
+	gameRectangle.body.setImmovable();
+	return gameRectangle;
+    }
 
-		makeBall(description) {
-				const width = 8;
-				const height = 8;
-				const color = 0xccccff;
-				let ball = this.physics.add.existing(
-						this.add.rectangle(
-								description.x,
-								description.y,
-								width,
-								height,
-								color
-						)
-				);
+    velocity() {
+	let speed = 100 * Phaser.Math.Between(1.3, 2);
+	if(Phaser.Math.Between(0, 10) > 5) {
+	    speed = 0 - speed;
+	}
+	return speed
+    }
+    
+    makeBall(description) {
+	const width = 8;
+	const height = 8;
+	const color = 0xccccff;
+	let ball = this.physics.add.existing(
+	    this.add.rectangle(
+		description.x,
+		description.y,
+		width,
+		height,
+		color
+	    )
+	);
+	ball.body.setBounce(1);
+	ball.body.setCollideWorldBounds(true);
+	ball.body.setVelocity(this.velocity(), 10)
+	return ball;
+    }
 
-		}
+    moving (keypress) {
+	if(keypress.left.isDown){
+	    this.platform[0].body.setVelocityX(-300);
+	}
+	else if (keypress.right.isDown) {
+	    this.platform[0].body.setVelocityX(300);
+	}
+	else {
+	    this.platform[0].body.setVelocityX(0);
+	}
+    }
 
-		moving (keypress) {
-				if(keypress.left.isDown){
-						this.platform[0].body.setVelocityX(-300);
-				}
-				else if (keypress.right.isDown) {
-						this.platform[0].body.setVelocityX(300);
-				}
-				else {
-						this.platform[0].body.setVelocityX(0);
-				}
-		}
+    colliders() {
+	this.physics.world.setBoundsCollision(true, true, true, true);
+	this.physics.add.collider(this.balls[0], this.platform[0]);
+	
+    }
 
-		
-		preload(){
-				this.load.image('background', 'assets/background01.png');
-		}
-		
-		create(){
-				this.add.image(185, 350, 'background');
-				this.platform[0] = this.makeRectangle({
-						x: 185,
-						y:650,
-						width: 80,
-						color: 0x00aaee
-				});
-				
-				this.mainBall = this.makeBall({x: 185, y: 670});
+    
 
-				this.cursors = this.input.keyboard.createCursorKeys(); 
-		}
+    
+    preload(){
+	this.load.image('background', 'assets/background01.png');
+    }
+    
+    create(){
+	this.add.image(0, 0, 'background').setOrigin(0, 0);
+	this.platform[0] = this.makeRectangle({
+	    x: 175,
+	    y:570,
+	    width: 80,
+	    color: 0xfaea50
+	});
+	
+	this.balls[0] = this.makeBall({x: 175, y: 100});
+	this.colliders();
 
-		update() {
-				this.moving(this.cursors, 0);
-		}
+	this.cursors = this.input.keyboard.createCursorKeys();
+
+	
+    }
+
+    update() {
+	this.moving(this.cursors, 0);
+    }
 }
--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 import { Game } from './game.js';
 
 const config = {
-		type: Phaser.CANVAS,
-		width: 375,
-		height:700,
-		scene: [Game],
-		parent: 'game',
-		physics: {
-				default: 'arcade',
-				arcade: {
-						gravity: { y: 400 },
-						debug: false
-				}
-		}
+    type: Phaser.CANVAS,
+    width: 350,
+    height:600,
+    scene: [Game],
+    parent: 'game',
+    physics: {
+	default: 'arcade',
+	arcade: {
+	    gravity: { y: 400 },
+	    debug: false
+	}
+    }
 }
 
 let game = new Phaser.Game(config);
