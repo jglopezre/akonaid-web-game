b5ee65ed4bd272241cb39c284658afa90269fbad
Javier Lopez <javierlopez_85@hotmail.com>
2021-11-28 17:34:00 -0300

    game over added

--- src/game.js
+++ src/game.js
@@ -1,152 +1,185 @@
 export class Game extends Phaser.Scene {
-	constructor() {
-		super({ key: 'game' });
-		this.platform = [];
-		this.balls = [];
-		this.startSpeed = -250;
-		this.cursors;
-		this.scoreText;
-	}
+    constructor() {
+        super({
+            key: 'game'
+        });
+        this.platform = [];
+        this.balls = [];
+        this.startSpeed = -250;
+        this.cursors;
+        this.scoreText;
+    }
 
-	makeBricks() {
-		this.bricks = this.physics.add.staticGroup({
-			key: ['whiteBrick', 'orangeBrick', 'cyanBrick', 'greenBrick'],
-			frameQuantity: 7,
-			gridAlign: {
-				width: 7,
-				height: 4,
-				cellWidth: 46,
-				cellHeight: 24,
-				x: 37,
-				y: 250
-			}
-		});
-	}
+    makeBricks() {
+        let bricks = this.physics.add.staticGroup({
+            key: ['whiteBrick', 'orangeBrick', 'cyanBrick', 'greenBrick'],
+            frameQuantity: 7,
+            gridAlign: {
+                width: 7,
+                height: 4,
+                cellWidth: 46,
+                cellHeight: 24,
+                x: 37,
+                y: 250
+            }
+        });
+        return bricks;
+    }
 
-	//Platform creating function
-	makePlatform(position) {
-		let platform = this.physics.add.sprite(position.x, position.y, 'playerPlatform');
-		platform.body.allowGravity = false;
-		platform.body.setImmovable();
-		platform.body.setCollideWorldBounds(true);
-		this.anims.create({
-			key: 'alive',
-			frames: this.anims.generateFrameNumbers('playerPlatform', { start: 0, end: 7 }),
-			frameRate: 4,
-			repeat: -1
-		});
-		return platform;
-	}
-
-
-	makeBall(position) {
-		let ball = this.physics.add.image(position.x, position.y, 'ball');
-		ball.body.setBounce(1);
-		ball.body.setCollideWorldBounds(true);
-		ball.setData('glue', true);
-		return ball;
-	}
-
-	moving(keypress) {
-		if (keypress.left.isDown) {
-			this.platform[0].body.setVelocityX(-300);
-		}
-		else if (keypress.right.isDown) {
-			this.platform[0].body.setVelocityX(300);
-		}
-		else {
-			this.platform[0].body.setVelocityX(0);
-		}
-	}
-
-	platformImpact(ball, platform) {
-
-		let relativeImpact = ball.x - platform.x;
-		if (relativeImpact > 0) {
-			ball.body.setVelocityX(10 * relativeImpact);
-		} else if (relativeImpact < 0) {
-			ball.body.setVelocityX(10 * relativeImpact);
-		} else {
-			ball.body.setVelocityX(Phaser.Math.Between(-10, 10));
-		}
+    //Platform creating function
+    makePlatform(position) {
+        let platform = this.physics.add.sprite(position.x, position.y, 'playerPlatform');
+        platform.body.allowGravity = false;
+        platform.body.setImmovable();
+        platform.body.setCollideWorldBounds(true);
+        this.anims.create({
+            key: 'alive',
+            frames: this.anims.generateFrameNumbers('playerPlatform', {
+                start: 0,
+                end: 7
+            }),
+            frameRate: 4,
+            repeat: -1
+        });
+        return platform;
+    }
 
 
-	}
+    makeBall(position) {
+        let ball = this.physics.add.image(position.x, position.y, 'ball');
+        ball.body.setBounce(1);
+        ball.body.setCollideWorldBounds(true);
+        ball.setData('glue', true);
+        return ball;
+    }
 
-	increasePoints(points) {
-		this.score += points;
-		this.scoreText.setText('SCORE: ' + this.score);
-	}
+    moving(keypress) {
+        if (keypress.left.isDown) {
+            this.platform[0].body.setVelocityX(-300);
+        } else if (keypress.right.isDown) {
+            this.platform[0].body.setVelocityX(300);
+        } else {
+            this.platform[0].body.setVelocityX(0);
+        }
+    }
 
-	brickImpact(ball, bricks) {
-		bricks.disableBody(true, true);
-		this.increasePoints(10);
-	}
+    platformImpact(ball, platform) {
 
-	colliders() {
-		this.physics.world.setBoundsCollision(true, true, true, false);
-		this.physics.add.collider(this.balls[0], this.platform[0], this.platformImpact, null, this);
-		this.physics.add.collider(this.balls[0], this.bricks, this.brickImpact, null, this);
+        let relativeImpact = ball.x - platform.x;
+        if (relativeImpact > 0) {
+            ball.body.setVelocityX(10 * relativeImpact);
+        } else if (relativeImpact < 0) {
+            ball.body.setVelocityX(10 * relativeImpact);
+        } else {
+            ball.body.setVelocityX(Phaser.Math.Between(-10, 10));
+        }
 
-	}
 
-	/*********SCENE DEFAULT METHODS*************************************************/
+    }
 
-	init() {
-		this.score = 0;
-	}
+    increasePoints(points) {
+        this.score += points;
+        this.scoreText.setText('SCORE: ' + this.score);
+    }
 
-	preload() {
-		this.load.image('background', 'assets/background02.png');
-		this.load.image('ball', 'assets/white-ball.png');
-		this.load.image('greenBrick', 'assets/green.png');
-		this.load.image('orangeBrick', 'assets/orange.png');
-		this.load.image('whiteBrick', 'assets/white.png');
-		this.load.image('cyanBrick', 'assets/cyan.png');
-		this.load.spritesheet('playerPlatform', 'assets/platform-blue-1.png', { frameWidth: 88, frameHeight: 22 });
+    brickImpact(ball, bricks) {
+        bricks.disableBody(true, true);
+        this.increasePoints(10);
+        if (this.bricks.countActive() === 0) {
+            this.congratsTetx.visible = true;
+        }
+    }
 
-	}
+    colliders() {
+        this.physics.world.setBoundsCollision(true, true, true, false);
+        this.physics.add.collider(this.balls[0], this.platform[0], this.platformImpact, null, this);
+        this.physics.add.collider(this.balls[0], this.bricks, this.brickImpact, null, this);
 
-	create() {
-		this.add.image(0, 0, 'background').setOrigin(0, 0);
+    }
 
-		this.scoreText = this.add.text(16, 16, 'SCORE: 0', {
-			fontSize: '20px',
-			fill: '#fff',
-			fontFamily: 'verdana, arial, sans-serif'
-		});
+    createTexts() {
+        this.congratsTetx = this.add.text(20, 300, 'ACOMPLISHED!!', {
+            fontSize: '40px',
+            fill: '#fff',
+        })
+        this.congratsTetx.visible = false;
 
-		this.platform[0] = this.makePlatform({ x: 175, y: 700 });
+        this.gameOverText = this.add.text(20, 300, 'GAME OVER', {
+            fontSize: '40px',
+            fill: '#fff',
+        })
+        this.gameOverText.visible = false;
+    }
 
-		this.balls[0] = this.makeBall({ x: 175, y: 684 });
+    /*********SCENE DEFAULT METHODS*************************************************/
 
-		this.cursors = this.input.keyboard.createCursorKeys();
+    init() {
+        this.score = 0;
+    }
 
-		this.makeBricks();
+    preload() {
+        this.load.image('background', 'assets/background02.png');
+        this.load.image('ball', 'assets/white-ball.png');
+        this.load.image('greenBrick', 'assets/green.png');
+        this.load.image('orangeBrick', 'assets/orange.png');
+        this.load.image('whiteBrick', 'assets/white.png');
+        this.load.image('cyanBrick', 'assets/cyan.png');
+        this.load.spritesheet('playerPlatform', 'assets/platform-blue-1.png', {
+            frameWidth: 88,
+            frameHeight: 22
+        });
 
-		this.colliders();
+    }
 
-	}
+    create() {
+        this.add.image(0, 0, 'background').setOrigin(0, 0);
 
-	update() {
-		this.platform[0].anims.play('alive', true);
-		this.moving(this.cursors, 0);
+        this.scoreText = this.add.text(16, 16, 'SCORE: 0', {
+            fontSize: '20px',
+            fill: '#fff',
+            fontFamily: 'verdana, arial, sans-serif'
+        });
 
-		if (this.balls[0].y > 755) {
-			console.log('GAME OVER');
-			this.scene.pause();
-		}
+        this.createTexts();
 
-		if (this.balls[0].getData('glue')) {
-			this.balls[0].x = this.platform[0].x;
-			if (this.cursors.up.isDown) {
-				if (this.balls[0].getData('glue')) {
-					this.balls[0].body.setVelocity((10 * Phaser.Math.Between(-30, 30)), this.startSpeed);
-					this.balls[0].setData('glue', false);
-				}
-			}
-		}
-	}
-	/****************************************************************************************/
-	/****************************************************************************************/
+        this.platform[0] = this.makePlatform({
+            x: 175,
+            y: 700
+        });
+
+        this.balls[0] = this.makeBall({
+            x: 175,
+            y: 684
+        });
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.bricks = this.makeBricks();
+
+        this.colliders();
+
+    }
+
+    update() {
+        this.platform[0].anims.play('alive', true);
+        this.moving(this.cursors, 0);
+
+        if (this.balls[0].y > 755) {
+            this.gameOverText.visible = true;
+            this.scene.pause();
+            this.bricks.setVisible(false);
+        }
+
+        if (this.balls[0].getData('glue')) {
+            this.balls[0].x = this.platform[0].x;
+            if (this.cursors.up.isDown) {
+                if (this.balls[0].getData('glue')) {
+                    this.balls[0].body.setVelocity((10 * Phaser.Math.Between(-30, 30)), this.startSpeed);
+                    this.balls[0].setData('glue', false);
+                }
+            }
+        }
+    }
+    /****************************************************************************************/
+    /****************************************************************************************/
 }
